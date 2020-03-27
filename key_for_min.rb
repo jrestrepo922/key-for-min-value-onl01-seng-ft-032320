02b0823325f41674bc66b658f608a72089d9e5e4
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-27 17:15:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-#hash = {:blake => 500, :ashley => 2, :adam => 1}
+hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   value_array = name_hash.collect { |name, value|
@@ -15,4 +15,5 @@ def key_for_min_value(name_hash)
   puts smallest_value
 end
 
+key_for_min_value(hash)
 
